76c247d1f43c027da3cf0bf4a7825775a37c7c6c
Yaroslav <yaroslav.iemelianov@gmail.com>
2018-02-17 20:08:32 +0300

    fix all

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ if USE_CUDA:
     word_input = word_input.cuda()
 encoder_outputs, encoder_hidden = encoder_test(word_input, encoder_hidden)
 
-word_inputs = Variable(torch.LongTensor([1, 2, 3])).view(1, -1)
+word_inputs = Variable(torch.LongTensor([1, 2, 3]))
 decoder_attns = torch.zeros(1, 3, 3)
 decoder_hidden = encoder_hidden
 decoder_context = Variable(torch.zeros(1, decoder_test.hidden_size))
@@ -29,6 +29,6 @@ if USE_CUDA:
     decoder_context = decoder_context.cuda()
 
 for i in range(3):
-    decoder_output, decoder_context, decoder_hidden, decoder_attn = decoder_test(word_inputs[i], decoder_context, decoder_hidden, encoder_outputs)
+    decoder_output, decoder_context, decoder_hidden, decoder_attn = decoder_test(word_inputs[i].view(1, -1), decoder_context, decoder_hidden, encoder_outputs)
     # print(decoder_output.size(), decoder_hidden.size(), decoder_attn.size())
     decoder_attns[0, i] = decoder_attn.squeeze(0).cpu().data

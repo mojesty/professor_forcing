ec43b996856b43d39d81e17845f8b92eca5fb3c6
yaroslav <yaroslav.iemelianov@gmail.com>
2018-02-24 13:18:00 +0300

    attention-based <UNK> filling done

--- cfg.py
+++ cfg.py
@@ -9,6 +9,7 @@ EOS_TOKEN = 'eos'
 UNK_TOKEN = 'unk'
 SOS_TOKEN_IDX = 0
 EOS_TOKEN_IDX = 1
+UNK_TOKEN_IDX = 2
 
 NEED_LOAD = False
 
--- eval.py
+++ eval.py
@@ -6,7 +6,7 @@ from torch.utils.data.dataloader import DataLoader
 from torch.autograd import Variable
 
 import cfg
-from cfg import MAX_LENGTH, USE_CUDA, SOS_TOKEN_IDX, EOS_TOKEN_IDX, EOS_TOKEN
+from cfg import MAX_LENGTH, USE_CUDA, SOS_TOKEN_IDX, EOS_TOKEN_IDX, EOS_TOKEN, UNK_TOKEN_IDX
 from dataset import QADataset
 
 vocab = torchtext.vocab.GloVe(name='840B', dim='300', cache='/media/data/nlp/wv/glove')
@@ -22,7 +22,7 @@ def evaluate(encoder, decoder, dataset_idx=None, sentence=None, max_length=MAX_L
     :return:
     """
     if dataset_idx is not None:
-        input_variable = Variable(qadataset[dataset_idx][0].unsqueeze(0))
+        input_variable = Variable(qadataset[dataset_idx][0].unsqueeze(0))  # 1 x instance_length
     elif sentence is not None:
         raise NotImplementedError
         input_variable = Variable(qadataset.indexes_from_sentence(sentence.split()[:MAX_LENGTH]))
@@ -62,7 +62,7 @@ def evaluate(encoder, decoder, dataset_idx=None, sentence=None, max_length=MAX_L
             break
         elif ni == UNK_TOKEN_IDX and fill_unks is not None:
             top_word, top_word_idx = decoder_attention.data.topk(1)
-            decoded_words.append(qadataset.itos(top_word.view(-1)))
+            decoded_words.append(qadataset.itos(input_variable.squeeze(0)[top_word_idx.view(-1)].cpu().data[0]))
         else:
             decoded_words.append(qadataset.itos(ni))
 
@@ -78,7 +78,7 @@ def main(i):
     print('Successfully loaded from disk')
     print('Sentence  {}'.format(qadataset.data[i]['context']))
     print('Question  {}'.format(qadataset.data[i]['question']))
-    words, _ = evaluate(encoder=encoder, decoder=decoder, dataset_idx=i, max_length=MAX_LENGTH)
+    words, _ = evaluate(encoder=encoder, decoder=decoder, dataset_idx=i, max_length=MAX_LENGTH, fill_unks='attn')
     print('Generated {}'.format(words))
 
 if __name__ == '__main__':
